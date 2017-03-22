efc72f3b5fc17f0edd207bffa444cd038b0369f0
Mario Colque <colquemario@gmail.com>
2017-03-22 04:30:36 -0300

    updated examples

--- example/rates-example.js
+++ example/rates-example.js
@@ -2,12 +2,30 @@
 
 const bitpayRates = require('../lib/rates')
 
+// Using Promises
+const ratesPromise = bitpayRates.get()
+ratesPromise.then(rates => {
+  console.log('Promise Rates:', rates)
+})
+.catch(err => {
+  console.log('Promise Error:', err)
+})
+
+const ratePromise = bitpayRates.get('ARS')
+ratePromise.then(rate => {
+  console.log('Promise Rate:', rate)
+})
+.catch(err => {
+  console.log('Promise Error:', err)
+})
+
+// Using Callbacks
 bitpayRates.get('ARS', (err, res) => {
-  console.log('Error:', err)
-  console.log('Rate:', res)
+  console.log('Callback Error:', err)
+  console.log('Callback Rate:', res)
 })
 
 bitpayRates.get((err, res) => {
-  console.log('Error:', err)
-  console.log('Rates:', res)
+  console.log('Callback Error:', err)
+  console.log('Callback Rates:', res)
 })
