25a496e5369001cd5cedbac0eaaa80349f9e9353
Mario Colque <mario@hashdog.com>
2019-02-27 00:07:00 -0300

    improve tests

---
+++ tests/rates.test.js
@@ -0,0 +1,30 @@
+const nock = require("nock");
+
+const bitpayRates = require("../lib/rates");
+const code = "ARS";
+
+describe("Get User tests", () => {
+  beforeEach(() => {
+    nock("https://bitpay.com")
+      .persist()
+      .get(`/rates/${code}`)
+      .reply(200, {
+        data: {
+          code
+        }
+      });
+  });
+
+  it("Get a rate by code using Promises", async () => {
+    const rate = await bitpayRates.get(code);
+    expect(typeof rate).toEqual("object");
+    expect(rate.code).toEqual(code);
+  });
+
+  it("Get a rate by code using Callback", () => {
+    bitpayRates.get(code, (err, rate) => {
+      expect(typeof rate).toEqual("object");
+      expect(rate.code).toEqual(code);
+    });
+  });
+});
