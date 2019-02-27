dc42413b4eafa199402d95244b9d490a92856eb0
Mario Colque <mario@hashdog.com>
2019-02-26 22:36:14 -0300

    update examples

--- example/rates-example.js
+++ example/rates-example.js
@@ -1,31 +1,23 @@
-'use strict'
-
-const bitpayRates = require('../lib/rates')
+const bitpayRates = require("../lib/rates");
 
 // Using Promises
-const ratesPromise = bitpayRates.get()
-ratesPromise.then(rates => {
-  console.log('Promise Rates:', rates)
-})
-.catch(err => {
-  console.log('Promise Error:', err)
-})
+const ratesPromise = bitpayRates.get();
+ratesPromise
+  .then(rates => console.log("Promise Rates:", rates))
+  .catch(err => console.log("Promise Error:", err));
 
-const ratePromise = bitpayRates.get('ARS')
-ratePromise.then(rate => {
-  console.log('Promise Rate:', rate)
-})
-.catch(err => {
-  console.log('Promise Error:', err)
-})
+const ratePromise = bitpayRates.get("ARS");
+ratePromise
+  .then(rate => console.log("Promise Rate:", rate))
+  .catch(err => console.log("Promise Error:", err));
 
 // Using Callbacks
-bitpayRates.get('ARS', (err, res) => {
-  console.log('Callback Error:', err)
-  console.log('Callback Rate:', res)
-})
+bitpayRates.get("ARS", (err, res) => {
+  console.log("Callback Error:", err);
+  console.log("Callback Rate:", res);
+});
 
 bitpayRates.get((err, res) => {
-  console.log('Callback Error:', err)
-  console.log('Callback Rates:', res)
-})
+  console.log("Callback Error:", err);
+  console.log("Callback Rates:", res);
+});
