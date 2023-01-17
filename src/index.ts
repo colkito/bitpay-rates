c0ae4ea9180e8b34d717c647a299d4403389fd45
colkito <github@colkito.com>
2023-01-16 22:42:34 -0300

    improve typing

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
-import * as https from 'https';
-import { IncomingMessage } from 'http';
+import https, { RequestOptions } from 'https';
 
-type EmptyObject = Record<string, never>;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Callback = (...args: any[]) => void;
-
-type RateObj = {
+export type RateObj = {
   code: string;
   name: string;
   rate: number;
 };
 
-type OptionsObj = {
-  host: string;
-  path: string;
-  headers: EmptyObject;
-  agent: boolean;
+export type RateResponse = RateObj | [RateObj];
+
+export type Callback = (error: Error | null, data?: RateResponse) => void;
+
+const defaultOptions: RequestOptions = {
+  host: 'bitpay.com',
+  path: '/rates',
+  headers: {},
+  agent: false,
 };
 
-const returnPromise = (options: OptionsObj): Promise<RateObj> => {
+const returnPromise = (options: RequestOptions): Promise<RateResponse> => {
   return new Promise((resolve, reject) => {
     returnCallback(options, (err, data) => {
       if (err) return reject(err);
-      return resolve(data);
+      return resolve(data as RateResponse);
     });
   });
 };
 
-const returnCallback = (options: OptionsObj, callback: Callback): void => {
+const returnCallback = (options: RequestOptions, callback: Callback): void => {
   https
-    .get(options, (res: IncomingMessage) => {
+    .get(options, (res) => {
       let dataBuffer = '';
 
       res.on('data', (chunk: Buffer) => {
@@ -42,35 +40,28 @@ const returnCallback = (options: OptionsObj, callback: Callback): void => {
           const { data } = JSON.parse(dataBuffer);
           return callback(null, data);
         } catch (err) {
-          return callback(err);
+          return callback(err as Error);
         }
       });
     })
-    .on('error', (err: Error) => {
-      return callback(err);
+    .on('error', (err) => {
+      return callback(err as Error);
     });
 };
 
 export const get = (
   code?: string | Callback,
   callback?: Callback,
-): Promise<RateObj> | Promise<[RateObj]> | void => {
-  const options = {
-    host: 'bitpay.com',
-    path: '/rates',
-    headers: {},
-    agent: false,
-  };
-
+): Promise<RateResponse> | void => {
+  const options = { ...defaultOptions };
+  if (typeof code === 'string') {
+    options.path += `/${code.toUpperCase()}`;
+  }
   if (typeof code === 'function') {
     return returnCallback(options, code);
-  } else if (typeof code === 'string') {
-    options.path += `/${code.toUpperCase()}`;
-    if (callback) {
-      return returnCallback(options, callback);
-    }
-    return returnPromise(options);
-  } else if (typeof code === 'undefined') {
+  } else if (callback) {
+    return returnCallback(options, callback);
+  } else {
     return returnPromise(options);
   }
 };
